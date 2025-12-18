9c2dd5bbb41a5876fed5ee799cb210ebe4e66c1b
For Humo <abduvoriskakhramonov@gmail.com>
2025-12-18 14:49:22 +0500

    Update and rename config.js to bot.js

--- config.js
+++
@@ -1,3 +0,0 @@
-export const BOT_TOKEN = "8504848437:AAFH4yzdrnPjjIZ2Bv2rnhMVupU5AmKPRVc";
-export const CHANNEL_USERNAME = "@ForHumoTV"; // kanal
-export const YT_CHANNEL_ID = "UCxxxxxxxxxxxx"; // ForHumo_eSport
